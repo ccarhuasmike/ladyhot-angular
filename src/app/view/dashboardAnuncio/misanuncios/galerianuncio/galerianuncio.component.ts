402bc777cde3f8035f80253fe4f8e448607a7903
ccarhuasmike <ccarhuas.mike@gmail.com>
2019-02-12 18:04:33 -0500

    sss

--- src/app/view/dashboardAnuncio/misanuncios/galerianuncio/galerianuncio.component.ts
+++ src/app/view/dashboardAnuncio/misanuncios/galerianuncio/galerianuncio.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
+import { Tbl_galeria_anuncio } from "../../../../Models/Tbl_galeria_anuncioModels";
+import { Observable } from 'rxjs';
 @Component({
     selector: 'app-editanuncio',
     templateUrl: './galerianuncio.component.html',
@@ -7,19 +9,35 @@ import { DomSanitizer } from '@angular/platform-browser';
 })
 export class GaleriaAnuncioComponent implements OnInit {
     private base64Image: string;
+    public ListGaleria: Tbl_galeria_anuncio[] = [];
     constructor(
         private domSanitizer: DomSanitizer
     ) { }
 
     ngOnInit() {
+        for (let index = 0; index <= 5; index++) {
+            const tbl_galeria_anuncio: Tbl_galeria_anuncio = {
+                id: index,
+                tx_filename: "",
+                tx_ruta_file: "",
+                tx_ruta_file_cort: "",
+                size_file: 0,
+                id_tipo_archivo: 0,
+                dt_fe_crea: new Date(),
+                id_anuncio: 0,
+                txt_ruta_virtuales: "",
+                txt_ruta_virtuales_cortada: ""
+            };
+            this.ListGaleria.push(tbl_galeria_anuncio);
+        }
+        console.log(this.ListGaleria);
+        //https://medium.com/@amcdnl/file-uploads-with-angular-reactive-forms-960fd0b34cb5
     }
 
-    onFileChange(event) {
+    onFileChange(event, id: number) {
         let reader = new FileReader();
         if (event.target.files && event.target.files.length) {
             const [file] = event.target.files;
-
-
             reader.onload = () => {
                 // this.formGroup.patchValue({
                 //     file: reader.result
@@ -34,11 +52,17 @@ export class GaleriaAnuncioComponent implements OnInit {
 
 
 
-    displayPhoto(fileInput) {
-        var fileDisplayArea = document.getElementById('fileDisplayArea');
+    displayPhoto(fileInput, id: number) {
         if (fileInput.target.files && fileInput.target.files[0]) {
-            const reader = new FileReader();
             debugger;
+            let object = this.ListGaleria.filter(x => {
+                x.id = id
+            });
+            console.log(object);
+
+
+
+            const reader = new FileReader();
             if (fileInput.target.files && fileInput.target.files.length > 0) {
                 let file = fileInput.target.files[0];
                 reader.onloadend = (e) => {

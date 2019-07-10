0d92935e04dbfd2e6db9953558d3bc870ada6ed4
mike <eduardo23@gmail.com>
2019-07-09 23:19:42 -0500

    asdadasd

--- src/app/view/panelcontrol/index/indexcomponent.ts
+++ src/app/view/panelcontrol/index/indexcomponent.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Router } from "@angular/router";
 import { debug } from 'util';
 // import { BehaviorSubject } from 'rxjs';

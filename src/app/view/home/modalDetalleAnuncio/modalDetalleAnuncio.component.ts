808a47057bbb51c37c623d7b150479634f9c43bf
danteccarhuas <ccarhuas.dante@gmail.com>
2019-07-21 20:39:18 -0500

    cambios para detalle anuncio

--- src/app/view/home/modalDetalleAnuncio/modalDetalleAnuncio.component.ts
+++ src/app/view/home/modalDetalleAnuncio/modalDetalleAnuncio.component.ts
@@ -29,13 +29,4 @@ export class ModalDetalleAnuncio implements OnInit {
     closeModal() {
         this.modalRef.hide();
     }
-
-    colapsar() {
-        this.isCollapsed = !this.isCollapsed;
-        if (this.isCollapsed) {
-            this.estiloColapsar = "style='display:block'";
-        } else {
-            this.estiloColapsar = "style='display:none'";
-        }
-    }
 }

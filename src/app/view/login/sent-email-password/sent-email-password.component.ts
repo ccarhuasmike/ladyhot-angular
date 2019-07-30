789b9e4b52a0fcfa14f41527be2ba713839bbff4
mike <eduardo23@gmail.com>
2019-07-29 20:36:16 -0500

    nuevos cambios de seguridad

---
+++ src/app/view/login/sent-email-password/sent-email-password.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+@Component({
+    selector: 'app-sent-email-password',
+    templateUrl: "./sent-email-password.component.html"
+})
+export class SendEmailPasswordComponent implements OnInit {
+    constructor(
+    ) { }
+
+    ngOnInit() {
+    }
+}

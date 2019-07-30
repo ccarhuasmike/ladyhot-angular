ec9acd152d2daa346c753b37fe3ab24aa5a0f7b4
mike <eduardo23@gmail.com>
2019-07-29 21:24:07 -0500

    routing de seguridad

--- src/app/view/login/login-routing.module.ts
+++ src/app/view/login/login-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { IngresarComponent } from './index/index.component';
-
+import { EscogerPasswordComponent } from './escoger-password/escoger-password.component';
+import { PasswordGuardadoComponent } from './password-guardado/password-guardado.component';
+import { RestablecerPasswordComponent } from './restablecer-password/restablecer-password.component';
+import { SendEmailComponent } from './sent-email/sent-email.component';
+import { SendEmailPasswordComponent } from './sent-email-password/sent-email-password.component';
 const routes: Routes = [
     {
         path: '',
@@ -11,6 +15,27 @@ const routes: Routes = [
     {
         path: 'ingresar',
         component: IngresarComponent,
+    }
+    ,
+    {
+        path: 'EscogerPassword',
+        component: EscogerPasswordComponent,
+    },
+    {
+        path: 'PasswordGuardado',
+        component: PasswordGuardadoComponent,
+    },
+    {
+        path: 'RestablecerPassword',
+        component: RestablecerPasswordComponent,
+    },
+    {
+        path: 'SendEmail',
+        component: SendEmailComponent,
+    },
+    {
+        path: 'SendEmailPassword',
+        component: SendEmailPasswordComponent,
     }
 ];
 

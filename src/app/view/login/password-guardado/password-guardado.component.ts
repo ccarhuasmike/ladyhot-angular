978d56c3f6a923564ffec65b61e97ead4516ecc3
mike <eduardo23@gmail.com>
2019-07-29 20:56:27 -0500

    passsword guardado

---
+++ src/app/view/login/password-guardado/password-guardado.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+@Component({
+    selector: 'app-password-guardado',
+    templateUrl: "./password-guardado.component.html"
+})
+export class PasswordGuardadoComponent implements OnInit {
+    constructor(
+    ) { }
+
+    ngOnInit() {
+    }
+}

f6a8cdd8138987a3a1bf05f91e657318718ee8ef
ccarhuasmike <ccarhuas.mike@gmail.com>
2020-02-01 17:51:32 -0500

    dfgfdg

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,14 +1,15 @@
-
 //open/close lateral filter    
 function cdFilter() {
     triggerFilter(true);
 }
+
 function cdClose() {
     triggerFilter(false);
 }
+
 function triggerFilter($bool) {
     var elementsToTrigger = $([$('.cd-filter-trigger'), $('.cd-filter'), $('.cd-tab-filter'), $('.cd-gallery')]);
-    elementsToTrigger.each(function () {
+    elementsToTrigger.each(function() {
         $(this).toggleClass('filter-is-visible', $bool);
     });
 }

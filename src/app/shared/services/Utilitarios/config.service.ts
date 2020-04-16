f5b39695b9dc1bdd99a0fe7288d9035b2d9b244c
danteccarhuas <ccarhuas.dante@gmail.com>
2020-04-16 12:41:10 -0500

    configuraciones para desplegar en hosting

--- src/app/shared/services/Utilitarios/config.service.ts
+++ src/app/shared/services/Utilitarios/config.service.ts
@@ -4,7 +4,7 @@ export class ConfigService {
     _webApi: string;
     constructor() {
         //this._webApi = "http://192.168.0.14/webapiladyhot/api/";
-        this._webApi = "http://localhost:7044/api/";
+        this._webApi = "http://danteccarhuas-001-site1.ftempurl.com/api/";
     }
     getWebApiURL() {
         return this._webApi;

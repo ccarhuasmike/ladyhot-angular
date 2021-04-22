1cca7a5d8e2c8d2f908965c9d62b89686ffa5bdb
ccarhuasmike <ccarhuas.mike@gmail.com>
2021-04-22 13:32:01 -0500

    ss

--- src/app/shared/services/Utilitarios/config.service.ts
+++ src/app/shared/services/Utilitarios/config.service.ts
@@ -3,8 +3,8 @@ import { Injectable } from '@angular/core';
 export class ConfigService {
     _webApi: string;
     constructor() {
-        //this._webApi = "http://192.168.0.14/webapiladyhot/api/";
-        this._webApi = "http://localhost:7044/api/";        
+        this._webApi = "http://192.168.0.31/webapiladyhot/api/";
+        //this._webApi = "http://localhost:7044/api/";        
         //this._webApi = "http://danteccarhuas-001-site1.ftempurl.com/api/";
     }
     getWebApiURL() {

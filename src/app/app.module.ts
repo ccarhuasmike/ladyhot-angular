6f2c059109ef22c17a56fc84727e77d8d96415cc
ccarhuasmike <ccarhuas.mike@gmail.com>
2021-06-01 22:46:20 -0500

    nuevos cambios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NotFoundComponent } from '../app/view/not-found/not-found.component';
 import { CoreModule } from '../app/view/core/core.module';
+
+import { HomeModule } from '../app/view/home/home.module';
+import { AboutModule } from '../app/view/about/about.module';
+import { PanelControlModule } from '../app/view/panelcontrol/panelcontrol.module';
+import { AnunciateGratisModule } from '../app/view/anunciategratis/anunciategratis.module';
+import { RedireccionarAnuncioModule } from '../app/view/redireccionar-anuncio/redireccionar-anuncio.module';
+
 import { SharedModule } from './shared/shared.module';
 // import { ModuleMapLoaderModule } from '@nguniversal/module-map-ngfactory-loader';
 @NgModule({
@@ -15,6 +22,13 @@ import { SharedModule } from './shared/shared.module';
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'gologolos' }),
+    
+    HomeModule,
+    AboutModule,
+    PanelControlModule,
+    AnunciateGratisModule,
+    RedireccionarAnuncioModule,
+
     AppRoutingModule,
     //HttpModule,
     HttpClientModule,

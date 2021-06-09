557145f743b8340efb1d78260035bd3a852b8b73
ccarhuasmike <ccarhuas.mike@gmail.com>
2021-06-09 11:51:53 -0500

    f

--- src/app/shared/services/Utilitarios/config.service.ts
+++ src/app/shared/services/Utilitarios/config.service.ts
@@ -7,9 +7,9 @@ export class ConfigService {
         //this._webApi = "http://192.168.0.31/webapiladyhot/api/";
         //this._webApi = "http://localhost:7044/api/";        
         //this._webApi = "http://danteccarhuas-002-site2.ftempurl.com/api/";
-        //this._webApi = "https://webapigologolos.gologolos.com/api/";
+        this._webApi = "https://webapigologolos.gologolos.com/api/";
         this._webDomain = "https://www.gologolos.com/";
-        this._webDomain = "http://localhost:4200/";
+        //this._webDomain = "http://localhost:4200/";
     }
     getWebApiURL() {
         return this._webApi;
